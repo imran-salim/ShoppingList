3bbc92e517d2e244bac6973d13fffd1fb48685b4
i8abyte <imransalim08@gmail.com>
2025-08-18 00:42:26 +1200

    Add a FoodItem model

---
+++ Models/FoodItem.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Models;
+
+public class FoodItem
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public required string Name { get; set; }
+
+    [Required]
+    [Range(1, 1000)]
+    public int Quantity { get; set; }
+}
